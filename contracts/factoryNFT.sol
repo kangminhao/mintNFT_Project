8577a3c42d26d909555440d63cd570a0510758a6
LeoKang <leo.kang@medicrowd.com.tw>
2021-12-21 15:30:55 +0800

    commit

---
+++ contracts/factoryNFT.sol
@@ -0,0 +1,30 @@
+// contracts/NFT.sol
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract FactoryNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Factory NFT", "FTN") {
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
---
+++ scripts/mint-nft.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/factoryNFT.sol/factoryNFT.json");
+const contractAddress = "0x69c0ef6e2ed9490450a477eeeac22af415babe0e";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  
+    //the transaction
+    const tx = {
+      'from': PUBLIC_KEY,
+      'to': contractAddress,
+      'nonce': nonce,
+      'gas': 500000,
+      'maxPriorityFeePerGas': 1999999987,
+      'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+    }
+
+    const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    
+    console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+
+}
+
+mintNFT("https://gateway.pinata.cloud/ipfs/QmbqjyK2ttFKLqPEJoyLnfhkQJT3q5yha7UYWaazJfSRgD");
